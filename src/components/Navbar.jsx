a28932a9fc80eeeec200afec9f6e7c20fc417634
Maciej Przestrzelski <przestrzelski.maciej1@gmail.com>
2022-10-19 19:37:50 +0200

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  height: 60px;
+`
+
+const Navbar = () => {
+  return (
+    <div>
+      <Container/>
+    </div>
+  )
+}
+
+export default Navbar
