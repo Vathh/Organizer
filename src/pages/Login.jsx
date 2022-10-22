1a6403bc04e3f269ddc7c8f62c1fb8231198964c
Maciej Przestrzelski <przestrzelski.maciej1@gmail.com>
2022-10-22 14:06:43 +0200

    login page done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,21 +5,32 @@ import {LOGIN_TYPES} from '../helpers/actionTypes'
 import { INITIAL_LOGIN_FORM_STATE, loginFormReducer } from '../helpers/loginFormReducer'
 import LogoSvg from '../img/OGARNIZER.svg'
 
+
 //#region STYLES
 const Container = styled.div`
   position: relative;
   height: 100vh;
   width: 100vw;
+  padding: 30px 0px;
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
-  background: rgb(53,6,94);
+  background: #35065e;
   background: linear-gradient(60deg, rgba(53,6,94,1) 0%, rgba(105,28,94,1) 65%, rgba(53,6,94,1) 100%);
   background-color: rgba(0,0,0,.5);
 `
+const Logo = styled.img`
+  height: 300px;
+  width: 300px;
+  justify-self: flex-start;
+  filter: invert(84%) sepia(21%) saturate(5849%) hue-rotate(351deg) brightness(105%) contrast(91%);
+`
 
-const LoginPannel = styled.div`
+const LoginPannel = styled.form`
+  position: relative;
+  width: 300px;
+  margin: 0 20px;
+  top: 50px;
   display: flex;
   flex-direction: column;
   background-color: rgba(0,0,0,.5);
@@ -29,50 +40,89 @@ const LoginPannel = styled.div`
 `
 
 const FormInput = styled.div`
-  padding: 30px 20px;
+  position: relative;
   display: flex;
   justify-content: space-between;
+  margin: 30px 20px; 
 `
 const Label = styled.label`
-  font-size: 16px;
-  padding: 5px 10px;
+  position: absolute;
+  left: 10px;
+  top: 14px;
+  transition: all .2s;
+  padding: 0 2px;
+  z-index: 1;
+
+  ::before{
+    content: "";
+    height: 5px;
+    background: rgba(105,28,94,1);
+    position: absolute;
+    left: 0;
+    top: 10px;
+    width: 100%;
+    z-index: -1;
+  }
 `
 
 const Input = styled.input`
-  font-size: 16px;  
-  border: none;
-  padding: 0 10px;
-  border-radius: 6px;
+  padding: 0.4rem;
+  width: 100%;
+  height: 100%;
+  border: 2px solid #3d1a48; 
+  border-radius: 5px;
   background: rgba(105,28,94,1);
+  font-size: 18px;
+  outline: none;
   color: white;
-  transition: transform .3fs;
+  transition: all .3s;
+  padding-left: 12px;
+
+  :focus{
+    border: 2px solid #cda619;
+
+    ::placeholder{
+      opacity: 1;
+      animation-delay: .2s;
+    }
+
+    +label{
+      top: -10px;
+      color: #cda619;
+    }
+  }  
+
+  :not(:placeholder-shown){
+    +label{
+      top: -10px;
+      color: #cda619;
+    }
+  }  
 
   ::placeholder{
     color: white;
-  }
-
-  :focus{
-    outline: none;
-    transform: scale(1.05);
+    font-size: 16px;
+    opacity: 0;
+    transition: all .3s;
   }
 `
 
 const SubmitBtn = styled.button`
-  margin: 20px 15px 15px;
+  margin: 20px 20px 15px;
   font-size: 18px;
-  padding: 9px 30px;
+  padding: 9px 0;
   border-radius: 10px;
-  background: rgb(37,67,170);
-  background: linear-gradient(60deg, #162864 0%, #6f1862 65%);
+  background: rgb(53,6,94);
+background: linear-gradient(60deg, rgba(53,6,94,1) 0%, rgba(105,28,94,1) 65%);
   color:white;
   border: none;
   background-size: cover;
-`
+  outline: none;
+  transition: transform .3s;
 
-const Logo = styled.img`
-  height: 300px;
-  width: 300px;
-  filter: invert(86%) sepia(21%) saturate(76%) brightness(50%) contrast(107%);
+  :focus{
+    transform: scale(1.1);
+  }
 `
 
 
@@ -115,13 +165,13 @@ const Login = () => {
       <Logo src={LogoSvg}/>
         <LoginPannel>
           <FormInput>
-            <Label>Login:</Label>
-            <Input type="text" placeholder='Login' onChange={handleChange} name={"login"}/>
+            <Input type="text" placeholder='Wpisz swój login' onChange={handleChange} name={"login"} />
+            <Label >Login</Label>
           </FormInput>
 
           <FormInput>
-            <Label>Hasło:</Label>
-            <Input type="text" placeholder='Hasło' onChange={handleChange} name={"password"}/>
+            <Input type="text" placeholder='Wpisz hasło' onChange={handleChange} name={"password"}/>
+            <Label>Hasło</Label>
           </FormInput>
           <SubmitBtn>Zaloguj się</SubmitBtn>
         </LoginPannel>
