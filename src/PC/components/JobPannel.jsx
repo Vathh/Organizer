0bd87fa8e0fedc33fe8509036522828b4d5ff8a6
Maciej Przestrzelski <przestrzelski.maciej1@gmail.com>
2022-12-05 21:01:43 +0100

    added button for addtask action, task actions menu

--- src/PC/components/JobPannel.jsx
+++ src/PC/components/JobPannel.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Avatar from '../../img/avatar.jpg'
 import MoreVertIcon from '@mui/icons-material/MoreVert';
@@ -70,20 +70,61 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
   `
 
   const NavBtn = styled.div`
-    
+    position: relative;
   `
 
   const IconStyles = {
     position: 'relative',
     top: '50%',
     transform: 'translateY(-50%)',
-    color: '#fff'
+    color: '#fff',
+    cursor: 'pointer'
   }
 
+  const NavMenu = styled.div`
+    position: absolute;
+    background-color: #420939;    
+    bottom: 100%;
+    right: 50%;
+    display: flex;
+    flex-direction: column;
+    border-radius: 10px;
+    padding: 7px 14px;
+    transition: .3s;
+    transform-origin: bottom;
+  `
 
+  const MenuVisible = {
+    transform: 'scaleY(1)'
+  }
+
+  const MenuInvisible = {    
+    transform: 'scaleY(0.5)'
+  }
+
+  const NavAction = styled.span`
+    font-size: 17px;
+    color: #fff;  
+    white-space: nowrap;
+    text-align: end;
+    padding-bottom: 5px;
+
+    :first-of-type{
+      padding-top: 5px;
+    }
+  `
+  
 //#endregion
 
 const JobPannel = () => {
+
+  const [menuVisibility, setMenuVisibility] = useState(false);
+
+  const handleMenuVisibility = (e) => {
+    setMenuVisibility(!menuVisibility);
+  }
+  
+
   return (
     <Container>
       <Description>Naprawa c253 <Priority /></Description>
@@ -93,8 +134,13 @@ const JobPannel = () => {
       <Category>Aktualizacja: <Info>Test test</Info></Category>
       <Navigation>
         <UserAvatar/>
-        <NavBtn>
+        <NavBtn onClick={handleMenuVisibility}>
           <MoreVertIcon style={IconStyles}/>
+          <NavMenu style={menuVisibility ? MenuVisible : MenuInvisible}>
+            <NavAction>Do fakturowania</NavAction>
+            <NavAction>Edytuj</NavAction>
+            <NavAction>Zamknij</NavAction>
+          </NavMenu>
         </NavBtn>
       </Navigation>
     </Container>
--- src/PC/pages/PCHome.jsx
+++ src/PC/pages/PCHome.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import Browser from '../components/Browser'
 import JobPannel from '../components/JobPannel'
+import AddIcon from '@mui/icons-material/Add';
 
 //#region STYLES
 
@@ -40,6 +41,30 @@ import JobPannel from '../components/JobPannel'
 
   const SubContainer = styled.div`
     flex: 1;
+    display: flex;
+    flex-direction: column;
+    
+    :nth-of-type(2){
+      margin: 0 25px;
+    }
+  `
+
+  const Header = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: space-between;
+    align-content: center;
+
+    ::after{
+      position: absolute;
+      content: '';
+      height: 80%;
+      top: 55%;
+      transform: translateY(-50%);
+      width: 1px;
+      background: #999;
+      right: 15%;
+    }
   `
 
   const TypeTitle = styled.span`
@@ -48,6 +73,15 @@ import JobPannel from '../components/JobPannel'
     font-family: 'Oswald', sans-serif;
   `
 
+  const AddIconStyles = {
+    position: 'relative',
+    top: '55%',
+    transform: 'translateY(-50%)',
+    color: '#fff',
+    fontSize: '35px',
+    cursor: 'pointer'
+  }
+
 //#endregion
 
 const PCHome = () => {
@@ -55,22 +89,32 @@ const PCHome = () => {
     <Container>
       <LeftSide>
         <PageTitle>Xanadu Serwis</PageTitle>
-        <Browser />
         <TasksContainer>
           <SubContainer>
-            <TypeTitle>Wyjazdy</TypeTitle>
+            <Header>
+              <TypeTitle>Wyjazdy</TypeTitle>
+              <AddIcon style={AddIconStyles}/>
+            </Header>
             <JobPannel />
           </SubContainer>
           <SubContainer>
-
+            <Header>
+              <TypeTitle>Serwisy</TypeTitle>
+              <AddIcon style={AddIconStyles}/>
+            </Header>
+            <JobPannel />
           </SubContainer>
           <SubContainer>
-
+            <Header>
+              <TypeTitle>Zamówienia</TypeTitle>
+              <AddIcon style={AddIconStyles}/>
+            </Header>
+            <JobPannel />
           </SubContainer>
         </TasksContainer>
       </LeftSide>
       <RightSide>
-
+        <Browser />
       </RightSide>
     </Container>
   )
